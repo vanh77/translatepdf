28251c63a83b4d0cfedc2f69a54ca23a8d1506c9
vanh77 <vanh7749h@gmail.com>
2026-07-27 22:56:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -109,7 +109,7 @@ if uploaded_file:
                     """
 
                     response = client.models.generate_content(
-                        model="gemini-2.5-flash", contents=[image, prompt]
+                        model="gemini-3.5-flash", contents=[image, prompt]
                     )
 
                     html_code = response.text
