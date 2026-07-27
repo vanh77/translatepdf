4a8744a88b9b452343c55f31a43b63abdc7acdcc
vanh77 <vanh7749h@gmail.com>
2026-07-27 22:29:10 +0700

    Refactor app.py to improve structure and functionality

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ if uploaded_file:
 
                     # Gọi API Gemini
                     response = client.models.generate_content(
-                        model="gemini-2.5-flash", contents=[image, prompt]
+                        model="gemini-3.5-flash", contents=[image, prompt]
                     )
 
                     html_code = response.text
